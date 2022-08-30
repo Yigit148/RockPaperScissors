38a507d5fa9db253e2ceadc16d941f5bc991b9cb
Yigit148 <tekes1481@gmail.com>
2022-08-30 16:27:00 +0300

    w

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,8 +1,5 @@
-
 user1 = input("What's your name?")
 user2 = input("And your name?")
-
-
 
 print("For " + user1 + " a = rock, s = paper, d = scissors")
 print("For " + user2 + " j= rock, k = paper, h = scissors")
@@ -11,32 +8,31 @@ a = j = "rock"
 s = k = "paper"
 d = h = "scissors"
 
-
 user1_answer = input("do yo want to choose rock, paper or scissors?")
 user2_answer = input("do you want to choose rock, paper or scissors?")
 
 
-
 def compare(u1, u2):
     if u1 == u2:
-        return("It's a tie!")
+        return ("It's a tie!")
     elif u1 == a:
         if u2 == h:
-            return("Rock wins!")
+            return ("Rock wins!")
         else:
-            return("Paper wins!")
+            return ("Paper wins!")
     elif u1 == d:
         if u2 == k:
-            return("Scissors win!")
+            return ("Scissors win!")
         else:
-            return("Rock wins!")
+            return ("Rock wins!")
     elif u1 == s:
         if u2 == j:
-            return("Paper wins!")
+            return ("Paper wins!")
         else:
-            return("Scissors win!")
+            return ("Scissors win!")
     else:
-        return("Invalid input! You have not entered rock, paper or scissors, try again.")
+        return ("Invalid input! You have not entered rock, paper or scissors, try again.")
+
 
 print(compare(user1_answer, user2_answer))
 
